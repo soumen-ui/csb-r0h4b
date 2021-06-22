fb2aa04990ff7a77eae2300743e88e8b1e9cb487
Code Sandbox <hello@codesandbox.io>
2021-06-22 14:50:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.434c21b6bb6c9f8374dff0b8460f041b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84dbe1dc26cf4314d91a2eec880413c",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/index.html"
+  },
+  {
+    "revision": "714cbe5c10aef7d30605",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/css/main.44646a49.chunk.css"
+  },
+  {
+    "revision": "a988f095e7fd2473edd2",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/js/2.a2d9cf46.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/js/2.a2d9cf46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714cbe5c10aef7d30605",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/js/main.6c70afed.chunk.js"
+  },
+  {
+    "revision": "434edcafcbed65f5ab1d",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/js/runtime-main.ec57d42e.js"
+  },
+  {
+    "revision": "a8eb40b5c730c843ca3d6e3c1af4ba82",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/media/bnn1.a8eb40b5.jpg"
+  },
+  {
+    "revision": "10bf26fb01353bec026623e9720622f2",
+    "url": "https://soumen-ui.github.io/csb-r0h4b/static/media/gg.10bf26fb.jpg"
+  }
+]);
